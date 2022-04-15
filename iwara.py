4d758060ca365e1866ed39fd81867a21712113cf
zuijiasy <64622956+zuijiasy@users.noreply.github.com>
2022-04-15 22:34:15 +0800

    Update iwara.py

--- iwara.py
+++ iwara.py
@@ -1,4 +1,4 @@
-import urllib.request
+import requests
 from lxml import etree
 import youtube_dl
 import sys
@@ -8,22 +8,28 @@ def iwaradownloader(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
     }
-    request = urllib.request.Request(url=url,headers=headers)
-    response = urllib.request.urlopen(request,timeout=30)
-    xmlselector = etree.HTML(response.read().decode('utf-8'))
+    requests.packages.urllib3.disable_warnings()
+    proxies = {
+        'http' : 'http://10.10.10.1:7890',
+        'https' : 'http://10.10.10.1:7890'
+    }
+    response = requests.get(url=url,headers=headers,verify=False,proxies=proxies)
+    xmlselector = etree.HTML(response.content.decode('utf-8'))
     author = xmlselector.xpath('//h1[@class]')
     username = str(author[0].text)[0:-9]
     lists = xmlselector.xpath('//h3/a/@href')
     dlist = []
     for a in lists:
         dlist.append("https://ecchi.iwara.tv" + a)
+    print(dlist)
     ydl_opt ={
         'outtmpl': '.\\' + username + '\\' + '%(title)s.%(ext)s',
-        'proxy' : 'http://127.0.0.1:7890',
+        'proxy' : 'http://10.10.10.1:7890',
         'format' : 'best'
     }
     with youtube_dl.YoutubeDL(ydl_opt) as ydl:
         result = ydl.download(dlist)
+
 
 if __name__ =='__main__':
     urls = ["https://ecchi.iwara.tv/users/m9F/videos" , "https://ecchi.iwara.tv/users/Ponkanman/videos"]
